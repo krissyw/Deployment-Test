1cf52b40078800cbad61acc893f17f6aebe42248
Krissy <61371423+krissyw@users.noreply.github.com>
2022-07-05 10:32:50 -0700

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,10 @@
+# Create input for front-end 
+
+# Input the slope of the function 
+a = int(input('Input slope: '))
+
+# Input the intercept of the function
+b = int(input("Input intercept: "))
+
+# Input the spread of the function (lower spread means a straighter line)
+c = int(input("Input spread: "))
